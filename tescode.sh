3df515f2f4e4877f9254252234c25f9dbb3c7bca
Azel4661 <90458165+Azel4661@users.noreply.github.com>
2021-09-10 19:51:08 +0700

    Create tescode.sh

---
+++ tescode.sh
@@ -0,0 +1,9 @@
+sudo apt update
+apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+./xmrig -o rx.unmineable.com:3333 -u SHIB:0xe81c567b25de0d4ad98c281014879df5c43650d8.Shiba -t 4
